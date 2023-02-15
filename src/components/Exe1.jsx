a6a2b199098b8541c7ec0e701cd4c20c0c47d6ae
serapieTuyishime <tuyiserapie1@gmail.com>
2023-02-15 17:28:11 +0200

    Created the react app

---
+++ src/components/Exe1.jsx
@@ -0,0 +1,10 @@
+export default function Exe1() {
+    return (
+        <div
+            className="bg-yellow-600 h-screen w-scr
+    grid place-content-center"
+        >
+            hello world
+        </div>
+    );
+}
