282afa3343cfb2eeb5d853aebf4b92d108223148
serapieTuyishime <tuyiserapie1@gmail.com>
2023-02-15 19:42:03 +0200

    Added a form

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Exe2 from "./components/Exe2";
 import Exe3 from "./components/Exe3";
 import Exe4 from "./components/Exe4";
 import Exe5 from "./components/Exe5";
+import Exe7 from "./components/Exe7";
 
 function App() {
     return (
@@ -13,7 +14,8 @@ function App() {
             {/* <Exe2 /> */}
             {/* <Exe3 /> */}
             {/* <Exe4 /> */}
-            <Exe5 />
+            {/* <Exe5 /> */}
+            <Exe7 />
         </div>
     );
 }
---
+++ src/components/Exe7.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+export default function Exe7() {
+    const [firstName, setFirstName] = useState("");
+    const [lastname, setLastName] = useState("");
+    return (
+        <div className="main">
+            <form className="grid gap-4">
+                <input
+                    className="form-input"
+                    value={firstName}
+                    onChange={(event) => {
+                        setFirstName(event.currentTarget.value);
+                    }}
+                    placeholder="First name"
+                />
+                <input
+                    className="form-input"
+                    placeholder="Last name"
+                    value={lastname}
+                    onChange={(event) => {
+                        setLastName(event.currentTarget.value);
+                    }}
+                />
+                <button
+                    onClick={() => alert(`Hello ${firstName} ${lastname}`)}
+                    className="bg-red-600 px-4 py-2 uppercase"
+                >
+                    Greet me
+                </button>
+            </form>
+        </div>
+    );
+}
