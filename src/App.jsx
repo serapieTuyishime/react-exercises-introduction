9e0856e6e0454af4bbfbf18421463767d562c467
serapieTuyishime <tuyiserapie1@gmail.com>
2023-02-15 19:03:58 +0200

    Exercise 5 done with mapping the cats and gods and horses

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Exe1 from "./components/Exe1";
 import Exe2 from "./components/Exe2";
 import Exe3 from "./components/Exe3";
 import Exe4 from "./components/Exe4";
+import Exe5 from "./components/Exe5";
 
 function App() {
     return (
@@ -11,7 +12,8 @@ function App() {
             {/* <Exe1 /> */}
             {/* <Exe2 /> */}
             {/* <Exe3 /> */}
-            <Exe4 />
+            {/* <Exe4 /> */}
+            <Exe5 />
         </div>
     );
 }
