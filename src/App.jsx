ce3833a8ec93bd2e6dc08da2ceb82d87af6541d0
serapieTuyishime <tuyiserapie1@gmail.com>
2023-02-15 18:39:42 +0200

    Exercise 4 done now i know how to use states

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import "./App.css";
 import Exe1 from "./components/Exe1";
 import Exe2 from "./components/Exe2";
 import Exe3 from "./components/Exe3";
+import Exe4 from "./components/Exe4";
 
 function App() {
     return (
         <div className="App">
             {/* <Exe1 /> */}
             {/* <Exe2 /> */}
-            <Exe3 />
+            {/* <Exe3 /> */}
+            <Exe4 />
         </div>
     );
 }
