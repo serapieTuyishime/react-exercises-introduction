a481d5012b1dc34368509fc937c80789e7fe692d
serapieTuyishime <tuyiserapie1@gmail.com>
2023-02-20 12:22:02 +0200

    created a change

--- src/components/Exe1.jsx
+++ src/components/Exe1.jsx
@@ -1,9 +1,6 @@
 export default function Exe1() {
     return (
-        <div
-            className="bg-yellow-600 h-screen w-scr
-    grid place-content-center"
-        >
+        <div className="grid w-screen h-screen bg-yellow-600 place-content-center">
             hello world
         </div>
     );
