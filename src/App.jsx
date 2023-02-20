13d28a1deda232bb0f80b9a98c0d51d123c6e6b1
serapieTuyishime <tuyiserapie1@gmail.com>
2023-02-20 10:04:34 +0200

    fixing initial route to be the first exercise

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <App />,
+        element: <Exe1 />,
     },
     {
         path: "/click1",
