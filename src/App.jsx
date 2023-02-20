3ea23525e0dfaecc073fbcc167ffac3de8b1570a
serapieTuyishime <tuyiserapie1@gmail.com>
2023-02-20 14:35:32 +0200

    Created changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,9 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Exe1 from "./components/Exe1";
-import Exe2 from "./components/Exe2";
-import Exe3 from "./components/Exe3";
-import Exe4 from "./components/Exe4";
-import Exe5 from "./components/Exe5";
-import Exe7 from "./components/Exe7";
 import Exe8 from "./components/Exe8";
+import "./App.css";
 
 function App() {
     return (
         <div className="App">
-            {/* <Exe1 /> */}
-            {/* <Exe2 /> */}
-            {/* <Exe3 /> */}
-            {/* <Exe4 /> */}
-            {/* <Exe5 /> */}
-            {/* <Exe7 /> */}
             <Exe8 />
         </div>
     );
